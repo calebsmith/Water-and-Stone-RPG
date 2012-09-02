5942dbf1c4c4c86d6cc83745ecd0f21b30acc825
Caleb Smith <csmith@caktusgroup.com>
2012-09-02 17:02:23 -0400

    Initial, and hopefully the last commit

---
+++ build.bat
@@ -0,0 +1,20 @@
+bc start.bas /e
+bc rpgtile.bas /e
+bc main.bas /e
+bc greet.bas /e
+bc battle.bas /e
+bc enter.bas /e
+bc load.bas /e
+bc save.bas /e
+bc magic.bas /e
+bc equip.bas /e
+link start.obj
+link rpgtile.obj
+link main.obj
+link greet.obj
+link battle.obj
+link enter.obj
+link load.obj
+link save.obj
+link magic.obj
+link equip.obj
